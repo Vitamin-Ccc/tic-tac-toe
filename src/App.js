40f3e6118f6bdd1c1c26cac1b4de442f965543de
Sky Lin <linsky519@gmail.com>
2022-06-23 01:26:08 -0600

    game board init setup done. Changing turn is working, need to check win

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+const initBoard = ['', '', '', '', '', '', '', '', '']
 function App() {
+  const [board, setBoard] = useState(initBoard)
+  const [xTurn, setXTurn] = useState(true)
+
+  const checkWin = () => {
+    console.log('board', board)
+  }
+  const handleClick = (index) => {
+    console.log(`clicked item: ${index}`)
+    // check game piece = ""
+    if (board[index] !== "") {
+      // stops the function here
+      return
+    }
+    // set game piece
+    // RULE: with react state, you can't directly mutate state
+    // you need to go through setstate to change state
+    // board[index] = xTurn ? "X" : "O"       BAD
+    let boardClone = [...board]
+    boardClone[index] = xTurn ? "X" : "O"     // This doesn't mutate state
+    setBoard(boardClone)
+    setXTurn(!xTurn)
+    // check win
+    checkWin()
+  }
+  const renderBoard = () => {
+    return board.map((piece, index) => {
+      return <div key={index} onClick={() => handleClick(index)} className="box">{piece}</div>
+    })
+  }
+
+  const reset = () => {
+    setBoard(initBoard)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={reset}>Reset</button>
+      <div className="game-board">
+        {renderBoard()}
+      </div>
     </div>
   );
 }
