931a1de5be5b93882f83be0c1370c432b15923c1
Sky Lin <linsky519@gmail.com>
2022-06-23 14:16:32 -0600

    app is functioning well, need to work on styling and maybe add more features

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ const initBoard = ['', '', '', '', '', '', '', '', '']
 function App() {
   const [board, setBoard] = useState(initBoard)
   const [xTurn, setXTurn] = useState(true)
+  const [winner, setWinner] = useState(null)
 
-  const checkWin = () => {
-    console.log('board', board)
+  const checkWin = (currentBoard) => {
+    let winner = null
+    // board is not updated yet
+    // console.log('board', currentBoard)
+    let winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8],
+    ];
+    for(let i = 0; i < winningCombos.length; i++) {
+      let [first, second, third] = winningCombos[i]
+      if (currentBoard[first] !== "" && currentBoard[first] === currentBoard[second] && currentBoard[first] === currentBoard[third]){
+        winner = currentBoard[first]
+        break
+      }
+    }
+    return winner
   }
   const handleClick = (index) => {
-    console.log(`clicked item: ${index}`)
+    // console.log(`clicked item: ${index}`)
     // check game piece = ""
-    if (board[index] !== "") {
+    if (board[index] !== "" || winner) {
       // stops the function here
       return
     }
@@ -22,10 +43,18 @@ function App() {
     // board[index] = xTurn ? "X" : "O"       BAD
     let boardClone = [...board]
     boardClone[index] = xTurn ? "X" : "O"     // This doesn't mutate state
+
+
+    // console.log('about to setBoard with boardClone', boardClone)
+    // async, board is not set yet, it will be by the time it renders
     setBoard(boardClone)
-    setXTurn(!xTurn)
     // check win
-    checkWin()
+    if(checkWin(boardClone)){
+      let winner = xTurn ? "X" : "O"
+      setWinner(winner)
+    } else{
+      setXTurn(!xTurn)
+    }
   }
   const renderBoard = () => {
     return board.map((piece, index) => {
@@ -35,6 +64,8 @@ function App() {
 
   const reset = () => {
     setBoard(initBoard)
+    setWinner(null)
+    setXTurn(false)
   }
   return (
     <div className="App">
@@ -43,6 +74,7 @@ function App() {
       <div className="game-board">
         {renderBoard()}
       </div>
+      {winner && <h1>{winner} won!</h1>}
     </div>
   );
 }
